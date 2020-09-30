a9f44b1d8a9eca78978924c41cb5eb8f25e7d362
argierdfj <argierincon@gmail.com>
2020-09-30 11:50:50 -0500

    Añadiendo contador

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import PrimeraApp from './PrimerApp';
+// import PrimeraApp from './PrimerApp';
+import CounterApp from './components/Counter/Counter'
 
 const App = document.querySelector('#app');
 
-ReactDOM.render( < PrimeraApp / > , App)
+ReactDOM.render( < CounterApp value={10} / > , App)
 // console.log(App);
