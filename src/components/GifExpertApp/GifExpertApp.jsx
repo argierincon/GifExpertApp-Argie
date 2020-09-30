9ce6b382a05550fe42a956ac5a911282d555b287
argierdfj <argierincon@gmail.com>
2020-09-30 16:15:48 -0500

    Añadiendo compenente título

---
+++ src/components/GifExpertApp/GifExpertApp.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './GifExpertApp.css'
+
+const GifExpertApp = () => {
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <hr />
+    </>
+  );
+};
+
+export default GifExpertApp;
+
+
+// const apiKey = 'CgPiOlDWJgWEGDo70vyz4ACQAac30Raj';
+
+// const getHTTP = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+// getHTTP.then( resp => {
+//   resp.json().then(data => {
+//     console.log(data)
+//   })
+// })
+// .catch(console.warn);
