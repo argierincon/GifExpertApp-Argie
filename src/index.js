207941ce723022dfa498783447917168f7c9ba2e
argierdfj <argierincon@gmail.com>
2020-09-30 12:29:23 -0500

    importando componente

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import PrimeraApp from './PrimerApp';
+
+import GifExpertApp from './components/GifExpertApp/GifExpertApp';
+
+import './index.css';
 
 const App = document.querySelector('#app');
 
-ReactDOM.render( < PrimeraApp value={10} / > , App)
+ReactDOM.render( < GifExpertApp / > , App)
