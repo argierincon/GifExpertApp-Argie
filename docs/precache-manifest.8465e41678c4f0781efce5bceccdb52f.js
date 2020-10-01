c57bbbaba9f4a2263db38f98bae7cef9898e540f
argierdfj <argierincon@gmail.com>
2020-10-01 12:48:17 -0500

    Renombrando build a docs

---
+++ docs/precache-manifest.8465e41678c4f0781efce5bceccdb52f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d26283b88e0398a8b231f54ecc5880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "535613576c7241d59df5",
+    "url": "/static/css/main.6465df34.chunk.css"
+  },
+  {
+    "revision": "dce2085711cc8ccf0542",
+    "url": "/static/js/2.aa7f8790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aa7f8790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535613576c7241d59df5",
+    "url": "/static/js/main.09741db6.chunk.js"
+  },
+  {
+    "revision": "b57b4c68968d7e3520e2",
+    "url": "/static/js/runtime-main.3a5e2f83.js"
+  }
+]);
