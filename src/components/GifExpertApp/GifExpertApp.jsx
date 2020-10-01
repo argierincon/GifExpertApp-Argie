9b99b536d4c41d305c1574853c7527db780a0669
argierdfj <argierincon@gmail.com>
2020-10-01 12:20:01 -0500

    Actualizando componente

--- src/components/GifExpertApp/GifExpertApp.jsx
+++ src/components/GifExpertApp/GifExpertApp.jsx
@@ -1,25 +1,24 @@
-import React from "react";
-import './GifExpertApp.css'
+import React, { useState } from "react";
+import AddCategory from "../AddCategory/AddCategory";
+import GifGrid from "../GifGrid/GifGrid";
+
+import "./GifExpertApp.css";
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["Anne"]);
+
   return (
     <>
       <h2>GifExpertApp</h2>
+      <AddCategory setCategories={setCategories} />
       <hr />
+      <ol>
+        {categories.map((category) => (
+          <GifGrid category={category} key={category} />
+        ))}
+      </ol>
     </>
   );
 };
 
 export default GifExpertApp;
-
-
-// const apiKey = 'CgPiOlDWJgWEGDo70vyz4ACQAac30Raj';
-
-// const getHTTP = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
-
-// getHTTP.then( resp => {
-//   resp.json().then(data => {
-//     console.log(data)
-//   })
-// })
-// .catch(console.warn);
